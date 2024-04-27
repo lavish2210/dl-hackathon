7dced57812eaebbaab92bc2242ed9b030953ea31
Lavish Sachdeva <b20113@students.iitmandi.ac.in>
2024-04-27 12:14:41 +0530

    First Commit

---
+++ diffusion/home/views.py
@@ -0,0 +1,8 @@
+from django.shortcuts import render
+from django.http import JsonResponse 
+
+def home(request): 
+    return render(request, 'home.html')
+
+def get_image(request):
+    return JsonResponse({'image_url':'image.jpg'}, safe=False)
